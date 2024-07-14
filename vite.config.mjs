a27c30a5ee38fbfb67f949dc68a35a7e5b0c680f
moistbeans23 <uno59648@gmail.com>
2024-07-14 14:21:58 -0600

    fix: fix vercel/vite settings config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: "dist",
   },
   plugins: [react(),],
 
